c83babe1ebfbd93ec4c4f9a9e9f62bb766a491ad
Sreek <sreekanthmr@yahoo.com>
2024-02-05 09:59:07 +0530

    Removed default error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cors-check';
+  searchRes!: string;
+  constructor(private searchService: SearchService) {}
+  ngOnInit() {
+    console.log('inside ngOninit');
+    this.search();
+    //throw new Error('Method not implemented.');
+  }
+  
+  search() {
+    console.log('testing search');
+    this.searchService.makeSearch();
+  }
 }
+
---
+++ src/app/search.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor() { }
+
+  async makeSearch() {
+    console.log('inside make search');
+    let data = JSON.stringify({
+      "context": {
+        "domain": "retail:1.1.0",
+        "location": {
+          "country": {
+            "code": "IND"
+          },
+          "city": {
+            "code": "std:080"
+          }
+        },
+        "action": "search",
+        "version": "1.1.0",
+        "bap_id": "ps-bap-network.becknprotocol.io",
+        "bap_uri": "https://ps-bap-network.becknprotocol.io",
+        "transaction_id": "beb65a81-6967-4c12-baa4-f1c03896a2b9",
+        "message_id": "619aad85-472a-4814-a7b7-3863064aa84d",
+        "timestamp": "2024-02-02T18:02:93+05:30"
+      },
+      "message": {
+        "intent": {
+          "item": {
+            "descriptor": {
+              "name": "sencor"
+            }
+          }
+        }
+      }
+    });
+    
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://ps-bap-client.becknprotocol.io/search',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    try {
+      const response = await axios.request(config);
+      console.log(JSON.stringify(response.data));
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+}
